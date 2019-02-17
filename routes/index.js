2bea439def9ce6a175960f213b9c6568387cbb0b
Chris Manganaro <chris.manganaro@gmail.com>
2019-02-17 09:11:36 +0800

    Tidy up and extra sounds

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   fs.readdir('./sounds/', (err, files) => {
-    res.render('index', { title: 'Stonecutters Soundboard', files: files });
+    const replace = {};
+
+    files.forEach(f => {
+      replace[path.extname(f)] = '';
+    });
+
+    res.render('index', {
+      title: 'Stonecutters Soundboard',
+      files: files,
+      replace: replace
+    });
   });
 });
 
