9d47a1fa5a459fbd3fe24d9dd228cf7a30a22b37
rbransby <rob@bransby.com.au>
2019-02-19 02:17:06 +0000

    forcing to use omxplayer and bluetooth

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var player = require('play-sound')((opts = {}));
+var player = require('play-sound')((opts = {player:'omxplayer'}));
 
 /* GET users listing. */
 router.post('/playsound', function(req, res, next) {
-  player.play('./sounds/' + req.body.sound, function(err) {
+  console.log(req);
+  player.play('./sounds/' + req.body.sound, {omxplayer: ['-o', 'alsa:bluealsa']}, function(err) {
     if (err) {
       res.json({ played: false });
       throw err;
