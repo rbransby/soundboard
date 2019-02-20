e2bd6b0f28457b16c72c0efc86a285d4b350ba09
rbransby <rob@bransby.com.au>
2019-02-20 07:54:30 +0000

    switching to vlc

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var player = require('play-sound')((opts = {player:'omxplayer'}));
+var player = require('play-sound')((opts = {player:'cvlc'}));
 
 /* GET users listing. */
 router.post('/playsound', function(req, res, next) {
   console.log(req);
-  player.play('./sounds/' + req.body.sound, {omxplayer: ['-o', 'alsa:bluealsa']}, function(err) {
+  player.play('./sounds/' + req.body.sound,{cvlc: ['--play-and-exit']}, function(err) {
     if (err) {
       res.json({ played: false });
       throw err;
